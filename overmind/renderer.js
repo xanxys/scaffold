0aa83b3aa87ddd9354ad68a085e9e283fb373c0b
xanxys <xanxys@gmail.com>
2017-07-19 22:59:07 +0900

    Free endpoint viewmodel mock.

--- overmind/renderer.js
+++ overmind/renderer.js
@@ -3,18 +3,18 @@ const prim_color = 0x3498db;
 // Scaffold inferred / target world model.
 // Assumes z=0 is floor.
 class ScaffoldModel {
+
     constructor() {
         const unit = 0.06;
-        this.zofs = 0.03;
 
         this.rails = [{
             type: "RS",
-            center: new THREE.Vector3(0, 0, 0),
+            center: new THREE.Vector3(0, 0, 0.03),
             ori: new THREE.Vector3(0, 0, 1),
             id: 0,
         }, {
             type: "RS",
-            center: new THREE.Vector3(0, unit, 0),
+            center: new THREE.Vector3(0, unit, 0.03),
             ori: new THREE.Vector3(0, 0, 1),
             id: 1,
         }];
@@ -29,17 +29,29 @@ class ScaffoldModel {
             builder: {
                 dump: 'RH',
                 driver: false,
-            }
+            },
+            id: 1,
         }];
     }
+
+    get_worker_pos() {}
+
+    get_points() {
+        return [{
+            open: true,
+            pos: new THREE.Vector3(0, 0, 0.03),
+            normal: new THREE.Vector3(0, 0, 1)
+        }, {
+            open: true,
+            pos: new THREE.Vector3(0, 0.06 * 2, 0.03),
+            normal: new THREE.Vector3(0, 0, 1)
+        }];
+      }
 }
 
 // ViewModel / View.
 // Since we update all frame all the time, we don't care about MVVM framework / bindings etc.
 class View3DClient {
-    // TODO: Might be better to use CGS unit system instead of SI, since
-    // 1. physijs doc is poor
-    // 2. official examples assume cgs-like unit system, and exhibts slightly stable behavior
     constructor() {
         var _this = this;
 
@@ -74,14 +86,17 @@ class View3DClient {
         this.add_table();
 
         this.cad_models = {};
-        let load_state = {total: 4, loaded: 0};
+        let load_state = {
+            total: 4,
+            loaded: 0
+        };
         _.each(['S60C-T', 'S60C-RS', 'S60C-RR', 'S60C-RH'], (name) => {
             _this.stl_loader.load('./models/' + name + '.stl', (geom) => {
                 geom.scale(1e-3, 1e-3, 1e-3);
                 this.cad_models[name] = geom;
                 load_state.loaded++;
                 if (load_state.loaded === load_state.total) {
-                  this.regen_scaffold_view();
+                    this.regen_scaffold_view();
                 }
             });
         });
@@ -117,7 +132,7 @@ class View3DClient {
         _.each(this.model.rails, rail => {
             let mesh = new THREE.Mesh(this.cad_models['S60C-' + rail.type]);
             mesh.material = new THREE.MeshLambertMaterial({});
-            mesh.position.copy(rail.center.clone().add(new THREE.Vector3(0, 0, this.model.zofs)));
+            mesh.position.copy(rail.center);
             this.scene.add(mesh);
         });
         _.each(this.model.workers, worker => {
@@ -128,64 +143,21 @@ class View3DClient {
             mesh.position.z = this.model.zofs;
             this.scene.add(mesh);
         });
-    }
 
-    calc_power_net() {
-        let cubes = [];
-        _.each(this.scene.children, (elem) => {
-            if (elem.type !== "Mesh" || elem.userData["cube"] !== true) {
+        _.each(this.model.get_points(), point => {
+            if (!point.open) {
                 return;
             }
-            cubes.push(new ImmutableCube(elem));
-        });
-        let edges = [];
-        _.each(cubes, cube => edges.push.apply(edges, cube.get_edges()));
-
-        // Calculate contacts.
-        let pairs = [];
-        _.each(edges, (e0, i0) => {
-            _.each(edges.slice(i0 + 1, edges.length), (e1, i1) => {
-                if (e0.touches(e1)) {
-                    pairs.push([e0, e1]);
-                }
+            let mesh = new THREE.Mesh();
+            mesh.geometry = new THREE.SphereBufferGeometry(0.006);
+            mesh.material = new THREE.MeshBasicMaterial({
+                color: 'red',
+                opacity: 0.5,
+                transparent: true,
             });
+            mesh.position.copy(point.pos);
+            this.scene.add(mesh);
         });
-        console.log("edge-edge contacts", pairs.length);
-
-        // Add visualizing objects.
-
-        _.each(pairs, pair => {
-            this.add_ephemeral_edge_highlight(pair[0]);
-            this.add_ephemeral_edge_highlight(pair[1]);
-        });
-
-        // _.each(edges, e => this.add_ephemeral_edge_highlight(e));
-
-    }
-
-    add_ephemeral_edge_highlight(edge) {
-        let mat = new THREE.LineBasicMaterial({
-            color: "hotpink",
-            linewidth: 5,
-        });
-
-        let geom = new THREE.Geometry();
-        geom.vertices.push(edge.p0);
-        geom.vertices.push(edge.p1);
-
-        let obj = new THREE.Line(geom, mat);
-        this.scene.add(obj);
-
-        let t0 = Date.now();
-        let iv_id = window.setInterval(() => {
-            let age_sec = (Date.now() - t0) * 1e-3;
-            if (age_sec < 10) {
-                mat.color.multiplyScalar(0.9);
-            } else {
-                this.scene.remove(obj);
-                window.clearInterval(iv_id);
-            }
-        }, 500);
     }
 
     add_table() {
