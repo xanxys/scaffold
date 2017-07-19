28ec6282e608938d7e3cb2e640fe02442bf2577c
xanxys <xanxys@gmail.com>
2017-07-19 23:11:57 +0900

    Update package desc.

--- overmind/main.js
+++ overmind/main.js
@@ -1,59 +1,204 @@
-const electron = require('electron')
-  // Module to control application life.
-const app = electron.app
-  // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const prim_color = 0x3498db;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// Scaffold inferred / target world model.
+// Assumes z=0 is floor.
+class ScaffoldModel {
 
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: false
+    constructor() {
+        const unit = 0.06;
+
+        this.rails = [{
+            type: "RS",
+            center: new THREE.Vector3(0, 0, 0.03),
+            ori: new THREE.Vector3(0, 0, 1),
+            id: 0,
+        }, {
+            type: "RS",
+            center: new THREE.Vector3(0, unit, 0.03),
+            ori: new THREE.Vector3(0, 0, 1),
+            id: 1,
+        }];
+
+        this.workers = [{
+            type: 'builder',
+            loco: {
+                sensor: 123,
+                on_rail: 0,
+                pos: 0.3,
+            },
+            builder: {
+                dump: 'RH',
+                driver: false,
+            },
+            human_id: 1,
+            hw_id: "a343fd"
+        }];
     }
-  })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+    get_worker_pos() {}
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    get_points() {
+        return [{
+            open: true,
+            pos: new THREE.Vector3(0, 0, 0.03),
+            normal: new THREE.Vector3(0, 0, 1)
+        }, {
+            open: true,
+            pos: new THREE.Vector3(0, 0.06 * 2, 0.03),
+            normal: new THREE.Vector3(0, 0, 1)
+        }];
+      }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// ViewModel / View.
+// Since we update all frame all the time, we don't care about MVVM framework / bindings etc.
+class View3DClient {
+    constructor(model) {
+        var _this = this;
+        this.model = model;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 7);
+        this.camera.up = new THREE.Vector3(0, 0, 1);
+        this.camera.position.x = 0.3;
+        this.camera.position.y = 0.3;
+        this.camera.position.z = 0.3;
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+        this.scene = new THREE.Scene();
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+        let sunlight = new THREE.DirectionalLight(0xcccccc);
+        sunlight.position.set(0, 0, 1).normalize();
+        this.scene.add(sunlight);
+
+        this.scene.add(new THREE.AmbientLight(0x333333));
+
+        let bg = new THREE.Mesh(
+            new THREE.IcosahedronGeometry(5, 1),
+            new THREE.MeshBasicMaterial({
+                wireframe: true,
+                color: '#ccc'
+            }));
+        this.scene.add(bg);
+
+        this.texture_loader = new THREE.TextureLoader();
+        this.stl_loader = new THREE.STLLoader();
+
+        // Model derived things.
+        this.add_table();
+
+        this.cad_models = {};
+        let load_state = {
+            total: 4,
+            loaded: 0
+        };
+        _.each(['S60C-T', 'S60C-RS', 'S60C-RR', 'S60C-RH'], (name) => {
+            _this.stl_loader.load('./models/' + name + '.stl', (geom) => {
+                geom.scale(1e-3, 1e-3, 1e-3);
+                this.cad_models[name] = geom;
+                load_state.loaded++;
+                if (load_state.loaded === load_state.total) {
+                    this.regen_scaffold_view();
+                }
+            });
+        });
+
+        this.scaffold_view = new THREE.Object3D();
+        this.scene.add(this.scaffold_view);
+
+        // start canvas
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+
+        this.renderer.setSize(800, 600);
+        this.renderer.setClearColor('#aac');
+        $('#viewport').append(this.renderer.domElement);
+
+        this.controls = new THREE.TrackballControls(this.camera, this.renderer.domElement);
+        this.controls.noZoom = false;
+        this.controls.noPan = false;
+        this.controls.zoomSpeed = 0.1;
+        this.controls.maxDistance = 2;
+
+        $(window).resize(() => {
+            _this.update_projection();
+        });
+
+        this.update_projection();
+    }
+
+    regen_scaffold_view() {
+        this.scaffold_view.remove(this.scaffold_view.children);
+
+        _.each(this.model.rails, rail => {
+            let mesh = new THREE.Mesh(this.cad_models['S60C-' + rail.type]);
+            mesh.material = new THREE.MeshLambertMaterial({});
+            mesh.position.copy(rail.center);
+            this.scene.add(mesh);
+        });
+        _.each(this.model.workers, worker => {
+            let mesh = new THREE.Mesh(this.cad_models['S60C-T']);
+            mesh.material = new THREE.MeshLambertMaterial({
+                color: prim_color
+            });
+            mesh.position.z = this.model.zofs;
+            this.scene.add(mesh);
+        });
+
+        _.each(this.model.get_points(), point => {
+            if (!point.open) {
+                return;
+            }
+            let mesh = new THREE.Mesh();
+            mesh.geometry = new THREE.SphereBufferGeometry(0.006);
+            mesh.material = new THREE.MeshBasicMaterial({
+                color: 'red',
+                opacity: 0.5,
+                transparent: true,
+            });
+            mesh.position.copy(point.pos);
+            this.scene.add(mesh);
+        });
+    }
+
+    add_table() {
+        let table_material = new THREE.MeshLambertMaterial({
+            map: this.texture_loader.load('texture/wood.jpg')
+        });
+
+        table_material.map.wrapS = table_material.map.wrapT = THREE.RepeatWrapping;
+        table_material.map.repeat.set(2, 2);
+
+        const table_thickness = 0.03;
+        let table = new THREE.Mesh(new THREE.BoxGeometry(1, 1, table_thickness), table_material);
+        table.position.z = -table_thickness / 2;
+        table.receiveShadow = true;
+        this.scene.add(table);
+    }
+
+    update_projection() {
+        //this.renderer.setSize($('#viewport').width(), $('#viewport').height());
+        this.camera.aspect = 800 / 600; // $('#viewport').width() / $('#viewport').height();
+        this.camera.updateProjectionMatrix();
+    }
+
+    /* UI Utils */
+    animate() {
+        // note: three.js includes requestAnimationFrame shim
+        let _this = this;
+        requestAnimationFrame(function() {
+            _this.animate();
+        });
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+}
+
+let model = new ScaffoldModel();
+let client = new View3DClient();
+client.animate();
+
+new Vue({
+  el: '#workers',
+  data: model,
+});
