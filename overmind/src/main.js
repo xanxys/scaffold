386e0f1d1a7e84ed7a764bc0def3cc8360dd44b1
xanxys <xanxys@gmail.com>
2017-12-09 15:28:24 +0900

    Minimize .js code

--- overmind/src/main.js
+++ overmind/src/main.js
@@ -1,18 +1,11 @@
+// Initialize bootstrap.
 import $ from 'jquery';
-
 global.jQuery = $;
 require('bootstrap');
 
-import Vue from 'vue/dist/vue.js';
-import { WorkerBridge } from './comm.ts';
-import WorkerPool from './worker-pool.ts';
-import { WorldView, WorldViewModel } from './view-3d-client.ts';
-
-import * as THREE from 'three';
-import { ScaffoldModel, S60RailStraight, S60RailHelix, S60RailRotator, ScaffoldThing, S60RailFeederWide, S60TrainBuilder } from './scaffold-model';
-import {FeederPlanner1D} from './planner.ts';
-
 // Components
+import Vue from 'vue/dist/vue.js';
+
 import TheToolbar from './the-toolbar.vue';
 Vue.component('the-toolbar', TheToolbar);
 
@@ -28,73 +21,6 @@ Vue.component('timeline', Timeline);
 import TheTabWorker from './the-tab-worker.vue';
 Vue.component('the-tab-worker', TheTabWorker);
 
-const bridge = new WorkerBridge();
-
-// TODO: Move to the-toolbar.vue
-function flash_status() {
-    let el = $('#conn-icon');
-    el.addClass('text-muted');
-    setTimeout(() => {
-        el.removeClass('text-muted');
-    }, 100);
-}
-
-const workerPool = new WorkerPool();
-const model = new ScaffoldModel();
-
-function initModel() {
-    let rs = new S60RailStraight();
-    rs.coord.unsafeSetParent(model.coord, new THREE.Vector3(0, 0, 0.02));
-    model.things.push(rs);
-
-    let fd = new S60RailFeederWide();
-    fd.coord.unsafeSetParent(model.coord, new THREE.Vector3(0.1, 0, 0));
-    model.things.push(fd);
-
-    let tb = new S60TrainBuilder();
-    tb.coord.unsafeSetParent(model.coord, new THREE.Vector3(0, 0, 0.1));
-    model.things.push(tb);
-
-    const planner = new FeederPlanner1D(model, fd, tb);
-}
-initModel();
-
-const viewModel = new WorldViewModel(model, $('#add_rs'), $('#add_rh'), $('#add_rr'));
-
-let appVm = new Vue({
-    el: '#app',
-    data: {
-        bridge: bridge,
-        model: model,
-        workerPool: workerPool,
-        worldView: worldView,
-        worldViewModel: viewModel,
-    },
-});
-
-bridge.open(
-    br => {
-        appVm.$forceUpdate();
-    },
-    packet => {
-        flash_status();
-        if (packet.datagram !== null) {
-            workerPool.handleDatagram(packet);
-        }
-    });
-
-//// HUGE HACK
-// Something about WebGL needs this to be placed after new Vue(...).
-// Otherwise <canvas> will be inserted to DOM but shows nothing.
-// But worldview needs to be bound to the app component.
-// As a workaround, access internal data object of the component instance to bind later.
-const worldView = new WorldView(model, $(window), $('#viewport'), viewModel);
-viewModel.bindView(worldView);
-worldView.startRenderer();
-worldView.start();
-
-appVm._data.worldView = worldView;
-
-
-// For console debugging.
-global.sendCommand = (c, a) => bridge.sendCommand(c, a);
+// Hand over exec to TypeScript land.
+import { runMain } from './main.ts';
+runMain();
---
+++ overmind/src/main.ts
@@ -0,0 +1,84 @@
+import * as $ from 'jquery';
+
+import * as Vue from 'vue/dist/vue.js';
+import { WorkerBridge } from './comm';
+import WorkerPool from './worker-pool';
+import { WorldView, WorldViewModel } from './view-3d-client';
+
+import * as THREE from 'three';
+import { ScaffoldModel, S60RailStraight, S60RailHelix, S60RailRotator, ScaffoldThing, S60RailFeederWide, S60TrainBuilder } from './scaffold-model';
+import { FeederPlanner1D } from './planner';
+
+
+export function runMain() {
+    const bridge = new WorkerBridge();
+
+    // TODO: Move to the-toolbar.vue
+    function flash_status() {
+        let el: any = $('#conn-icon');
+        el.addClass('text-muted');
+        setTimeout(() => {
+            el.removeClass('text-muted');
+        }, 100);
+    }
+
+    const workerPool = new WorkerPool();
+    const model = new ScaffoldModel();
+
+    function initModel() {
+        let rs = new S60RailStraight();
+        rs.coord.unsafeSetParent(model.coord, new THREE.Vector3(0, 0, 0.02));
+        model.addRail(rs);
+
+        let fd = new S60RailFeederWide();
+        fd.coord.unsafeSetParent(model.coord, new THREE.Vector3(0.1, 0, 0));
+        model.addRail(fd);
+
+        let tb = new S60TrainBuilder();
+        tb.coord.unsafeSetParent(model.coord, new THREE.Vector3(0, 0, 0.1));
+        model.addRail(tb);
+
+        const planner = new FeederPlanner1D(model, fd, tb);
+    }
+    initModel();
+
+    const viewModel = new WorldViewModel(model);
+
+    let appVm = new Vue({
+        el: '#app',
+        data: {
+            bridge: bridge,
+            model: model,
+            workerPool: workerPool,
+            worldView: null,
+            worldViewModel: viewModel,
+        },
+    });
+
+    bridge.open(
+        br => {
+            appVm.$forceUpdate();
+        },
+        packet => {
+            flash_status();
+            if (packet.datagram !== null) {
+                workerPool.handleDatagram(packet);
+            }
+        });
+
+    //// HUGE HACK
+    // Something about WebGL needs this to be placed after new Vue(...).
+    // Otherwise <canvas> will be inserted to DOM but shows nothing.
+    // But worldview needs to be bound to the app component.
+    // As a workaround, access internal data object of the component instance to bind later.
+    const worldView = new WorldView(model, $(window), $('#viewport'), viewModel);
+    viewModel.bindView(worldView);
+    worldView.startRenderer();
+    worldView.start();
+
+    appVm._data.worldView = worldView;
+
+
+    // For console debugging.
+    global.sendCommand = (c, a) => bridge.sendCommand(c, a);
+}
